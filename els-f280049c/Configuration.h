7c4bccaabd54e020cfa3a1eb1bad1835799d705d
Kent VanderVelden <kent.vandervelden@gmail.com>
2020-10-14 08:48:00 -0500

    Configured for James W.'s lathe

--- els-f280049c/Configuration.h
+++ els-f280049c/Configuration.h
@@ -40,7 +40,7 @@
 //================================================================================
 
 // For Imperial leadscrews: pitch in Threads Per Inch (TPI)
-#define LEADSCREW_TPI 12
+#define LEADSCREW_TPI 8
 
 // For metric leadscrews: pitch in hundredths of a millimeter (HMM)
 // Example: 200hmm = 2mm
@@ -64,8 +64,8 @@
 //================================================================================
 
 // Steps and microsteps
-#define STEPPER_MICROSTEPS 8
-#define STEPPER_RESOLUTION 200
+#define STEPPER_MICROSTEPS 1
+#define STEPPER_RESOLUTION 1000
 
 // Separate step and microstep settings for feed rates.  Redefine these if your
 // lathe has a separate feed drive train with a different ratio.
